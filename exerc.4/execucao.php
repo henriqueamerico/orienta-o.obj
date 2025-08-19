3d575863d9a39361463021f2268146b13d40af56
Henrique Américo <henriquelopesamerico@gmail.com>
2025-08-19 12:02:19 -0300

    Add files via upload

---
+++ exerc.4/execucao.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+
+
+
+
+$opcao = 0;
+
+do{
+    echo "\n\n----MENU-----\n";
+    echo "1- Cadastrar carro\n";
+    echo "2- Excluir carro\n";
+    echo "3- Listar carros\n";
+    echo "0- Sair\n";
+
+    
+}
