2db8265fb7eefa60d6b973666ce81a2bb44c76bf
Henrique Américo <henriquelopesamerico@gmail.com>
2025-08-19 09:48:23 -0300

    Add files via upload

--- execucao.php
+++ execucao.php
@@ -1,23 +1,53 @@
 <?php
-require_once("modelo/Aluno.php");
-require_once("modelo/Turma.php");
-$turma = new Turma();
-$turma->setNome("Turma 2025");
-$turma->setCurso("Técnico em Desenvolvimento de sistemas");
 
-for ($i=1; $i<=5; $i++) {
-    $aluno = new Aluno();
-    $aluno->setNome(readline("Insira o nome: "));
-     $aluno->setIdade(readline("Insira a idade: "));
-      $aluno->setTurma($turma);
+require_once("modelo/Estado.php");
+require_once("modelo/Cidade.php");
 
-      $turma->addAluno($aluno);
+//Objeto do tipo Estado
+$sc = new Estado();
+$sc->setNome("Santa Catarina");
+$sc->setSigla("SC");
 
+$pr = new Estado();
+$pr->setNome("Paraná");
+$pr->setSigla("PR");
+
+
+//Objeto do tipo Cidade
+$flo = new Cidade();
+$flo->setNome("Florianópolis");
+$flo->setQtdHabitantes(576.361);
+$flo->setAltitude(3);
+$flo->setEstado($sc);
+
+$blu = new Cidade();
+$blu->setNome("Blumenau");
+$blu->setQtdHabitantes(380.597);
+$blu->setAltitude(21);
+$blu->setEstado($sc);
+
+$foz = new Cidade();
+$foz->setNome("Foz do Iguaçu");
+$foz->setQtdHabitantes(295.500);
+$foz->setAltitude(200);
+$foz->setEstado($pr);
+
+$cas = new Cidade();
+$cas->setNome("Cascavel");
+$cas->setQtdHabitantes(364.104);
+$cas->setAltitude(781);
+$cas->setEstado($pr);
+
+$cidades = array($flo, $blu, $foz, $cas);
+
+foreach($cidades as $c){
+    printf(
+        "A cidade de %s, localizada no estado %s-%s, possui %d habitantes e uma altitude de %d metros.\n",
+        $c->getNome(),
+        $c->getEstado()->getNome(),
+        $c->getEstado()->getSigla(),
+        $c->getQtdHabitantes(),
+        $c->getAltitude()
+    );
 }
 
-echo "Dados da turma:\n";
-echo "Nome: " . $turma->getNome() . " | Curso: " . $turma->getCurso() . "\n";
-
-echo "Alunos:\n";
-foreach($turma->getAlunos() as $a)
-    echo " - " . $a->getNome() . " | " . $a->getIdade() . " anos\n";
