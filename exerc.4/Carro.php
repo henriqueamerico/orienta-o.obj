d26d8b5b3b414de1bfe0e04578858aa6871338eb
Henrique Américo <henriquelopesamerico@gmail.com>
2025-08-25 19:24:00 -0300

    Update Carro.php

---
+++ exerc.4/Carro.php
@@ -0,0 +1,52 @@
+<?php
+require_once("Fabricante.php");
+
+class Carro
+{
+    private string $modelo;
+    private int $anoFabricacao;
+    private Fabricante $fabricante;
+
+    //GETs e SETs
+
+    public function getModelo(): string
+    {
+        return $this->modelo;
+    }
+
+
+    public function setModelo(string $modelo): self
+    {
+        $this->modelo = $modelo;
+
+        return $this;
+    }
+
+
+    public function getAnoFabricacao(): int
+    {
+        return $this->anoFabricacao;
+    }
+
+
+    public function setAnoFabricacao(int $anoFabricacao): self
+    {
+        $this->anoFabricacao = $anoFabricacao;
+
+        return $this;
+    }
+
+
+    public function getFabricante(): Fabricante
+    {
+        return $this->fabricante;
+    }
+
+
+    public function setFabricante(Fabricante $fabricante): self
+    {
+        $this->fabricante = $fabricante;
+
+        return $this;
+    }
+}
